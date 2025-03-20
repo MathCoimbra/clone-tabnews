2243157171519fcd9e4d7303a10b4400f99cfc5d
Matheus Coimbra <86252346+MathCoimbra@users.noreply.github.com>
2025-03-20 00:40:16 +0000

    fix index text

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Você é apenas um grão de areia no meio do vasto universo, para se destacar, você irá errar às vezes, mas aprenderá no processo, e irá muito longe!</h1>
+}
+
+export default Home;
