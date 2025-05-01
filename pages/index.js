c31f2e7034fe7086fe23d75e90e10527c9fa1ca4
Matheus Coimbra <86252346+MathCoimbra@users.noreply.github.com>
2025-05-01 01:24:11 +0000

    change sentence

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é apenas um grão de areia no meio do vasto universo, para se destacar, você irá errar às vezes, mas aprenderá no processo, e irá muito longe!</h1>
+    return <h1>Você é apenas um grão de areia no meio do vasto universo, para se destacar, você irá errar às vezes, mas aprenderá no processo, e irá muito longe! 🚀</h1>
 }
 
 export default Home;
